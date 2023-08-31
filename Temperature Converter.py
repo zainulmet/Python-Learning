f805fa15cff883dd658d53180684c7e17724c881
Ahmad Zainul Abidin <142303196+zainulmet@users.noreply.github.com>
2023-09-01 02:00:35 +0800

    Add files via upload

---
+++ Temperature Converter.py
@@ -0,0 +1,22 @@
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5/9
+
+print("Temperature Converter")
+print("1. Celsius to Fahrenheit")
+print("2. Fahrenheit to Celsius")
+
+choice = input("Enter choice (1/2): ")
+
+if choice == '1':
+    celsius = float(input("Enter temperature in Celsius: "))
+    fahrenheit = celsius_to_fahrenheit(celsius)
+    print(f"{celsius}°C is equal to {fahrenheit}°F")
+elif choice == '2':
+    fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+    celsius = fahrenheit_to_celsius(fahrenheit)
+    print(f"{fahrenheit}°F is equal to {celsius}°C")
+else:
+    print("Invalid choice")
